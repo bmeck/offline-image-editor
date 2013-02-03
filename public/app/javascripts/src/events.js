e355d3edae52f3bc5c7a30fbff36a4a8eb87a2a4
Bradley Meck <bradley.meck@gmail.com>
2013-02-03 16:31:26 -0600

    [fix] labels

--- public/app/javascripts/src/events.js
+++ public/app/javascripts/src/events.js
@@ -95,9 +95,11 @@ module.exports = {
                index++;
             },
             openDialog('gallery-dialog', function (data, form) {
-               new img(form.querySelector('input:checked').value).read(function (err, data) {
+               var filename = form.querySelector('input:checked').value;
+               new img(filename).read(function (err, data) {
                   var image = new Image();
                   image.onload = function () {
+                     document.getElementById('save-name').value = filename;
                      newCanvas(image.naturalWidth, image.naturalHeight);
                      var c = document.getElementById('c');
                      c.getContext('2d').drawImage(image, 0, 0);
